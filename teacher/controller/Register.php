3463dd87f5ad5fd77e70da9525df490270961a1f
C-HL <62871733+C-HL@users.noreply.github.com>
2020-04-01 13:35:54 +0800

    Add files via upload

--- teacher/controller/Register.php
+++ teacher/controller/Register.php
@@ -2,36 +2,37 @@
 namespace app\teacher\controller;
 
 use think\Controller;
-// use app\teacher\validate\User as UserValidate;
+use think\Db;
+use app\teacher\validate\User as UserValidate;
 use app\teacher\model\Teacher as TeacherModel;
 
 class Register extends Controller
 {
 	public function register()
 	{
-		return $this->fetch('user/add');
+		return $this->fetch();
 	}
 
 	public function doRegister()
 	{
 		$data = input('post.');
-	 	// $val = new UserValidate();
-	 	// $result = $val->check($data);
-	 	// if(!$result){
-	 	// 	$this->error($val->getError());
-	 	// 	exit;
-	 	// }
+		$val = new UserValidate();
+		$result = $val->check($data);
+		if(!$result){
+			$this->error($val->getError());
+			exit;
+		}
 		$user = new TeacherModel($data);
 		// dump($user);die;
     	$res = $user->where('name',input('name'))->count();
 		if($res > 0){
-				$this->error('管理员已存在');
+				$this->error('用户已存在');
 		}else{
 			$ret = $user->allowField(true)->save();
 			if($ret){
-				$this->success('新增成功');
+				$this->success('注册成功','index/index');
 			}else{
-				$this->error('新增失败');
+				$this->error('注册失败');
 			}
 		}
 	}
--- teacher/controller/demo.php
+++ teacher/controller/demo.php
@@ -1,25 +1,12 @@
 <?php
-namespace app\teacher\controller;
-
-use think\Controller;
-use think\Db;
-use app\teacher\model\Teacher as TeacherModel;
-
-class Index extends Controller
-{
-	public function index()
-	{
 
 
-        return $this->fetch();
-	}
-
-	public function login(){
-
-			$data = input('post.');
+$data = input('post.');
 			$user = new TeacherModel(); 
 			$res = $user->where('name',$data['name'])->find();
 			//dump($res);die;
+
+			
 			if(empty($res)){
 				return $this->error('用户名不存在');
 			}elseif(!$res['password'] === md5($data['password'])){
@@ -29,16 +16,23 @@ class Index extends Controller
 					}else{
 						session('name',$data['name']);
 						$this->success('验证成功,登陆中','User/index');
-					} 
-					
-	}
+					}
 
-	public function logout()
-	{
-	//销毁session
-    session(NULL);
-    //跳转页面
-    $this->success('退出成功','index');
-	}
-	
-}
+
+
+			if ($res) {
+				if ($res['password'] === md5($data['password'])) {
+					session('name',$data['name']);
+				}else{
+					return $this->error('密码不正确');
+				}
+			}else{
+				return $this->error('用户名不存在');
+			}
+
+			if(!captcha_check($data['captcha'])){
+ 				//验证失败
+ 				$this->error('验证码不正确');
+			}else{
+				$this->success('验证成功,登陆中','User/index');
+			}
